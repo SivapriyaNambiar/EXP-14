b86203b6cae9570d3e5ab0e202e84836e8ce09d0
SivapriyaNambiar <9sivapriya@gmail.com>
2024-10-25 06:48:29 +0530

    Add files via upload

---
+++ 14A.cpp
@@ -0,0 +1,30 @@
+// NAME - SIVAPRIYA NAMBIAR
+// PRN - 23070123125 
+// EXPERIMENT - 14(A)  
+
+// Program to show single inheritance.
+
+# include <iostream>
+# include <string>
+using namespace std;
+
+class Uni
+{
+    public:
+    string uni= "Symbiosis";
+    void discipline()
+    {
+        cout << "Engineering" <<endl;
+    }
+};
+class Dep : public Uni
+{
+    public:
+    string dept= " Electronics & Telecommunication";
+};
+int main()
+{
+    Dep u1;
+    u1.discipline();
+    cout<<u1.uni + " "+u1.dept ;
+} 
